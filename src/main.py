4806a8bb5d2e0f9ae83a33662eb0aca2ecf40797
Christian Talmo <talmo.christian@gmail.com>
2015-03-06 22:03:10 +0200

    added error messages also and passed tests

--- src/main.py
+++ src/main.py
@@ -36,86 +36,135 @@ class Osallistuja:
 
     def toString(self):
         return "{} '{}' {} : {}".format(self.__id, self.__name, self.__sid, self.__map_series)
+def main():
+    '''
+    Ohjelma lukee ensin tiedoston osallistujat.txt, tämän jälkeen tiedoston ottelu.txt ja viimeiseksi kaikki ne tiedostot, joiden nimet tiedostossa ottelu.txt on lueteltuina. Tiedostot luetaan em. luettelossa esitetyssä järjestyksessä.
+    '''
 
-'''
-Ohjelma lukee ensin tiedoston osallistujat.txt, tämän jälkeen tiedoston ottelu.txt ja viimeiseksi kaikki ne tiedostot, joiden nimet tiedostossa ottelu.txt on lueteltuina. Tiedostot luetaan em. luettelossa esitetyssä järjestyksessä.
-'''
+    map_osallistujat = {} # map ossallistujoista
+    lajit = [] # lista lajeista
 
-map_osallistujat = {} # map ossallistujoista
-lajit = [] # lista lajeista
+    # luetaan lista
+    try:
+        file_osallistujat = open("osallistujat.txt", "r+")
+    except:
+        print "Virhe osallistujatiedoston lukemisessa."
+        return -1
 
-# luetaan lista
-file_osallistujat = open("osallistujat.txt", "r+")
-for line in file_osallistujat:
-    s = line.strip().split(";") # id ; nimi ; sarja
-    o = Osallistuja( s[0], s[1], s[2] )
-    map_osallistujat[ s[0] ] = o
-file_osallistujat.close()
+    rivi = -1
+    for line in file_osallistujat:
+        rivi += 1
+        s = line.strip().split(";") # id ; nimi ; sarja
+        if (len(s) != 3):
+            print "Virheellinen osallistujatiedosto: rivi '"+rivi+"' ei ole muodossa tunnus;nimi;sarja."
+            return -1
 
-# luetaan ottelu tiedot
-file_ottelut = open("ottelu.txt", "r+")
-for line in file_ottelut:
-    # pistetaan laji nimi talteen
-    laji = line[:line.find(".txt")]
-    lajit.append(laji)
+        if (s[0] in map_osallistujat):
+            print "Virheellinen osallistujatiedosto: tunnus "+s[0]+" toistuu."
+            return -1
 
-    # pomitaan lajin tiedot taman tiedostosta
-    # ja lisataan pisteet osallistjoille
-    file_ottelu = open(line.strip(), "r+")
-    for line in file_ottelu:
-        s = line.strip().split(";") # id ; pisteet
-        o = map_osallistujat[ s[0] ]
-        o.set_points(laji, s[1])
-    file_ottelu.close()
-file_ottelut.close()
+        o = Osallistuja( s[0], s[1], s[2] )
+        map_osallistujat[ s[0] ] = o
+    file_osallistujat.close()
+
+    # luetaan ottelu tiedot
+    try:
+        file_ottelut = open("ottelu.txt", "r+")
+    except:
+        print "Virhe ottelutiedoston lukemisessa."
+        return -1
+
+    for line in file_ottelut:
+        # pistetaan laji nimi talteen
+        laji = line[:line.find(".txt")]
+        lajit.append(laji)
+
+        # pomitaan lajin tiedot taman tiedostosta
+        # ja lisataan pisteet osallistjoille
+        try:
+            file_ottelu = open(line.strip(), "r+")
+        except:
+            print "Virhe lajitiedoston "+line.strip()+" lukemisessa."
+            return -1
+
+        rivi = -1
+        for subline in file_ottelu:
+            rivi += 1
+            s = subline.strip().split(";") # id ; pisteet
+            if (len(s) != 2):
+                print "Virheellinen lajitiedosto "+line.strip()+": rivi '"+rivi+"' ei ole muodossa tunnus;pisteet."
+                return -1
+
+            if not (s[0] in map_osallistujat):
+                print "Virheellinen lajitiedosto "+line.strip()+": tunnus "+s[0]+" puuttuu osallistujatiedostosta."
+                return -1
+
+            o = map_osallistujat[ s[0] ]
+
+            if (o.get_points(laji) != None):
+                print "Virheellinen lajitiedosto "+line.strip()+": tunnus " +s[0]+" toistuu."
+                return -1
+
+            o.set_points(laji, s[1])
+        file_ottelu.close()
+    file_ottelut.close()
 
 
-'''
-Syötetiedostot luettuaan ohjelma tulostaa CSV-muotoisen tiedoston tulokset.txt, johon tulee otsikkorivi sekä yksi rivi kutakin osallistujat.txt-tiedostossa ollutta osallistujaa kohden. Rivin alkuun tulevat osallistujan perustiedot osallistujat.txt-tiedostosta, sen jälkeen jokaisen lajin pistetiedot ja loppuriville tulee vielä yhteenvetotietoja kilpailijan koko seitsenottelusta.
-'''
+    '''
+    Syötetiedostot luettuaan ohjelma tulostaa CSV-muotoisen tiedoston tulokset.txt, johon tulee otsikkorivi sekä yksi rivi kutakin osallistujat.txt-tiedostossa ollutta osallistujaa kohden. Rivin alkuun tulevat osallistujan perustiedot osallistujat.txt-tiedostosta, sen jälkeen jokaisen lajin pistetiedot ja loppuriville tulee vielä yhteenvetotietoja kilpailijan koko seitsenottelusta.
+    '''
 
-tulokset = []
+    tulokset = []
 
-'''
-Otsikkorivin sisältö muodostuu seuraavasti: Ensimmäiseksi tulevat sarakkeet tunnus, nimi ja sarja, tämän jälkeen sarake jokaiselle lajille, ja lopuksi sarakkeet kokonaispisteet sekä lajeja. Oletetaan, että lajitiedostojen nimet ovat .txt-päätteisiä. Tulostiedoston otsikkorivillä käytetään lajin nimenä tiedoston nimen alkuosaa.
-'''
-str_otsikkorivi = "tunnus;nimi;sarja"
-for laji in lajit:
-    str_otsikkorivi += (";" + laji)
-str_otsikkorivi += ";kokonaispisteet;lajeja"
-
-'''
-Kunkin osallistujan riville tulee ensimmäisiin kolmeen sarakkeeseen tiedostosta osallistujat.txt luetut tiedot. Lajisarakkeisiin tulevat muista syötetiedostoista luetut pistemäärät aina asiaankuuluvan otsikon alle, tai jos kilpailijalta puuttuu suoritus kyseisestä lajista, merkitään sarakkeeseen "-". Rivin loppuun tulevat:
-
-    * sarakkeeseen kokonaispistemäärä kilpailijan suorittamien lajien pistemäärien summa
-    * sarakkeeseen lajeja suoritettujen lajien lukumäärä ja lajien kokonaislukumäärä merkkijonona, jossa niiden välissä on merkki "/" eli esimerkiksi "6/7", jos kilpailija on suorittanut kuusi lajia seitsemästä.
-'''
-for key in map_osallistujat.keys():
-    o = map_osallistujat.get(key)
-    #print "\n"
-    #print o.get_name()
-    ostr = ("" + o.get_id() + ";" + o.get_name() + ";" + o.get_sid())
-    total_points = 0;
-    total_series = 0;
+    '''
+    Otsikkorivin sisältö muodostuu seuraavasti: Ensimmäiseksi tulevat sarakkeet tunnus, nimi ja sarja, tämän jälkeen sarake jokaiselle lajille, ja lopuksi sarakkeet kokonaispisteet sekä lajeja. Oletetaan, että lajitiedostojen nimet ovat .txt-päätteisiä. Tulostiedoston otsikkorivillä käytetään lajin nimenä tiedoston nimen alkuosaa.
+    '''
+    str_otsikkorivi = "tunnus;nimi;sarja"
     for laji in lajit:
-        p = o.get_points(laji)
-        #print p
-        if p:
-            total_points += p
-            total_series += 1
-            ostr += (";" + str(p))
-        else:
-            ostr += ";-"
-    ostr += (";" + str(total_points))
-    ostr += (";" + str(total_series) + "/" + str(len(lajit)))
-    tulokset.append( ostr )
+        str_otsikkorivi += (";" + laji)
+    str_otsikkorivi += ";kokonaispisteet;lajeja"
 
-tulokset = sorted(tulokset)
+    '''
+    Kunkin osallistujan riville tulee ensimmäisiin kolmeen sarakkeeseen tiedostosta osallistujat.txt luetut tiedot. Lajisarakkeisiin tulevat muista syötetiedostoista luetut pistemäärät aina asiaankuuluvan otsikon alle, tai jos kilpailijalta puuttuu suoritus kyseisestä lajista, merkitään sarakkeeseen "-". Rivin loppuun tulevat:
 
-file_tulokset = open("tulokset.txt", "wb")
-file_tulokset.write( str_otsikkorivi + '\n' )
-for line in tulokset:
-    file_tulokset.write( bytes(line + '\n') )
-file_tulokset.close()
-print "Tulokset kirjoitettu tiedostoon tulokset.txt."
+        * sarakkeeseen kokonaispistemäärä kilpailijan suorittamien lajien pistemäärien summa
+        * sarakkeeseen lajeja suoritettujen lajien lukumäärä ja lajien kokonaislukumäärä merkkijonona, jossa niiden välissä on merkki "/" eli esimerkiksi "6/7", jos kilpailija on suorittanut kuusi lajia seitsemästä.
+    '''
+    for key in map_osallistujat.keys():
+        o = map_osallistujat.get(key)
+        #print "\n"
+        #print o.get_name()
+        ostr = ("" + o.get_id() + ";" + o.get_name() + ";" + o.get_sid())
+        total_points = 0;
+        total_series = 0;
+        for laji in lajit:
+            p = o.get_points(laji)
+            #print p
+            if p:
+                total_points += p
+                total_series += 1
+                ostr += (";" + str(p))
+            else:
+                ostr += ";-"
+        ostr += (";" + str(total_points))
+        ostr += (";" + str(total_series) + "/" + str(len(lajit)))
+        tulokset.append( ostr )
 
+    tulokset = sorted(tulokset)
+    
+    try:
+        file_tulokset = open("tulokset.txt", "wb")
+        file_tulokset.write( str_otsikkorivi + '\n' )
+        for line in tulokset:
+            file_tulokset.write( bytes(line + '\n') )
+        file_tulokset.close()
+        print "Tulokset kirjoitettu tiedostoon tulokset.txt."
+    except:
+        print "Virhe tulostiedoston kirjoittamisessa."
+        return -1
+
+    return 0 # exit success
+
+
+# run program
+main()
